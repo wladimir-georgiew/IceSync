61f397f52fc301f9d9f6705e26e691037f25f459
Vladimir Georgiev <vladimir.georgiev.dev@gmail.com>
2025-03-26 04:20:43 +0200

    Add project files.

---
+++ IceSync.WebApp/Controllers/WeatherForecastController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace IceSync.WebApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+
+    }
+}
